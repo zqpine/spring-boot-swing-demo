ccb15a3be9e9ec15d01288dbdbf2b961c31ae3f9
zqpine <30688559+zqpine@users.noreply.github.com>
2017-09-30 16:52:15 +0800

    Add files via upload

---
+++ src/main/java/hello/DemoFrame.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import javax.swing.*;
+public class DemoFrame extends JFrame {
+
+    private JLabel jLabel;
+    private JTextField jTextField;
+    private JButton jButton;
+
+    public DemoFrame() {
+        super();
+        this.setSize(300, 200);
+        this.getContentPane().setLayout(null);
+        this.add(getJLabel(), null);
+        this.add(getJTextField(), null);
+        this.add(getJButton(), null);
+        this.setTitle("HelloWorld");
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    private javax.swing.JLabel getJLabel() {
+        if (jLabel == null) {
+            jLabel = new javax.swing.JLabel();
+            jLabel.setBounds(34, 49, 53, 18);
+            jLabel.setText("Name:");
+        }
+        return jLabel;
+    }
+
+    private javax.swing.JTextField getJTextField() {
+        if (jTextField == null) {
+            jTextField = new javax.swing.JTextField();
+            jTextField.setBounds(96, 49, 160, 20);
+        }
+        return jTextField;
+    }
+
+    private javax.swing.JButton getJButton() {
+        if (jButton == null) {
+            jButton = new javax.swing.JButton();
+            jButton.setBounds(103, 110, 71, 27);
+            jButton.setText("OK");
+        }
+        return jButton;
+    }
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(DemoFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(DemoFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(DemoFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(DemoFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new DemoFrame().setVisible(true);
+            }
+        });
+    }
+
+}
